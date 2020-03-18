546e34ec58eb4916b5ef6c1a4249d58ed181ec93
up201706978 <up201706978@fe.up.pt>
2020-03-18 18:59:51 +0000

    signal a dar TEM UM sleep(2)

--- SOPE1/simpledu.c
+++ SOPE1/simpledu.c
@@ -7,69 +7,77 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <signal.h>
 
 #define DIRECTORY "."
-#define DIRECTORY_LINE "./"                     
-#define MAX_SIZE 1000                           // Maximum address size (not efficient)
-#define DIGITS_MAX 11                           // Maximum number of digits 
-#define IGNORE_1 "."                            // Used to ignore the current directory address
-#define IGNORE_2 ".."                           // Used to ignore the previous directory address
-#define VERBOSE 0                               // DEBUGGING
-#define CUSTOM_INF 2147483647                   // INFINITY (No need to call library math.h)      
+#define DIRECTORY_LINE "./"
+#define MAX_SIZE 1000         // Maximum address size (not efficient)
+#define DIGITS_MAX 11         // Maximum number of digits
+#define IGNORE_1 "."          // Used to ignore the current directory address
+#define IGNORE_2 ".."         // Used to ignore the previous directory address
+#define VERBOSE 0             // DEBUGGING
+#define CUSTOM_INF 2147483647 // INFINITY (No need to call library math.h)
 
 #define BLOCK_SIZE_STAT 512
 #define BLOCK_SIZE_PRINT 1024
 
 /* Global variables: Used the dictate the flags */
 
-typedef struct{
-    int allFiles_C;         // -a or --all                          (C)
-    int bytesDisplay_C;     // -b, --bytes                          (C)
-    int blockSize_C;        // -B or --block-size = SIZE            (C)
-    int countLink_C;        // -l, --count-links                    // Always activated
-    int dereference_C;      // -L, --dereference                    (C)
-    int separatedirs_C;     // -S, --separate-dirs                  (C)
-    int maxDepth_C;         // --max-depth = N                      (C)
-}cTags;
+typedef struct
+{
+    int allFiles_C;     // -a or --all                          (C)
+    int bytesDisplay_C; // -b, --bytes                          (C)
+    int blockSize_C;    // -B or --block-size = SIZE            (C)
+    int countLink_C;    // -l, --count-links                    // Always activated
+    int dereference_C;  // -L, --dereference                    (C)
+    int separatedirs_C; // -S, --separate-dirs                  (C)
+    int maxDepth_C;     // --max-depth = N                      (C)
+} cTags;
 
 cTags tags = {0};
 
 // Function prototypes:
 
-long int seekdirec(char* currentdir, int depth);
-int createProcess(char* currentdir, int depth);
+long int seekdirec(char *currentdir, int depth);
+int createProcess(char *currentdir, int depth);
 void printTags();
-long int sizeAttribution(struct stat* temp);
-long int dereferenceLink(char* workTable, int depth);
+long int sizeAttribution(struct stat *temp);
+long int dereferenceLink(char *workTable, int depth);
+void sigint_handler(int sig);
 
 // Function bodies:
 
 /* Accesses the link */
 
-int blockSize=512;
+int blockSize = 512;
 
-long int dereferenceLink(char* workTable, int depth){
+long int dereferenceLink(char *workTable, int depth)
+{
     struct stat status;
     long int sizeRep;
 
     char megaPath[MAX_SIZE];
     readlink(workTable, megaPath, MAX_SIZE - 1);
-    if( !lstat(megaPath, &status) ){
-        sizeRep =  sizeAttribution(&status);
-        if( depth > 0 ){
+    if (!lstat(megaPath, &status))
+    {
+        sizeRep = sizeAttribution(&status);
+        if (depth > 0)
+        {
             //printf("[%d]\t", depth);
             printf("%ld\t%s\n", sizeRep, workTable);
         }
         return sizeRep;
     }
-    else{
+    else
+    {
         perror("(dereferenceLink) Failure to access path : ");
         return -1;
     }
 }
 
 /* [Used mostly for debugging] - Prints tags */
-void printTags(){
+void printTags()
+{
     printf("Printing variables:\n");
     printf("tags.allFiles_C = %d\n", tags.allFiles_C);
     printf("tags.bytesDisplay_C = %d\n", tags.bytesDisplay_C);
@@ -81,47 +89,84 @@ void printTags(){
 }
 
 /* Attributes sizes based on the activated tags. */
-long int sizeAttribution(struct stat* temp){
+long int sizeAttribution(struct stat *temp)
+{
 
     float value;
 
-    if(tags.bytesDisplay_C){
-    	value = (temp->st_size) ;
+    if (tags.bytesDisplay_C)
+    {
+        value = (temp->st_size);
     }
-    else{
-	value = (temp->st_blocks * BLOCK_SIZE_STAT) ;
-    	value/=tags.blockSize_C;
+    else
+    {
+        value = (temp->st_blocks * BLOCK_SIZE_STAT);
+        value /= tags.blockSize_C;
 
-    	int out=value;
-    	if(value>out)
-	value++;
+        int out = value;
+        if (value > out)
+            value++;
     }
     return value;
 }
 
+void sigint_handler(int sig)
+{
+    // kill(0, SIGSTOP);
+    int i = -1;
+    while (i != 0 && i != 1)
+    {
+        printf("0 - Continue \n 1 - Terminate Program\n");
+        scanf("%d", &i);
+    }
+    if (i == 0)
+    {
+        kill(0, SIGCONT);
+    }
+    else
+    {
+        kill(0, SIGKILL);
+    }
+}
+
 /* Creates a process. */
-int createProcess(char* currentdir, int depth){
+int createProcess(char *currentdir, int depth)
+{
     fflush(stdout);
     int n, fd[2];
     pid_t pid;
     char digitsre[DIGITS_MAX];
     memset(digitsre, '\0', DIGITS_MAX);
 
-    if(pipe(fd) < 0){
+    if (pipe(fd) < 0)
+    {
         fprintf(stderr, "pipe error\n");
-        exit(1); 
+        exit(1);
     }
-    if ( (pid = fork()) < 0) {
+    if ((pid = fork()) < 0)
+    {
         fprintf(stderr, "fork error\n");
         exit(2);
     }
-    else if (pid > 0) { /* pai */
+    else if (pid > 0)
+    { /* pai */
+        // prepare the 'sigaction struct'
+        struct sigaction action;
+        action.sa_handler = sigint_handler;
+        sigemptyset(&action.sa_mask);
+        action.sa_flags = 0;
+
+        // install the handler
+        sigaction(SIGINT, &action, NULL);
+
         close(fd[1]); /* fecha lado emissor do pipe */
         wait(NULL);
         read(fd[0], digitsre, DIGITS_MAX);
         n = atoi(digitsre);
         close(fd[0]); /* fecha lado receptor do pipe */
-    } else { /* filho */
+    }
+    else
+    {                 /* filho */
         close(fd[0]); /* fecha lado receptor do pipe */
         depth--;
         n = seekdirec(currentdir, depth);
@@ -131,33 +176,37 @@ int createProcess(char* currentdir, int depth){
         exit(0);
     }
 
-    return n; 
+    return n;
 }
 
-void print(long int size,char* workTable){
-	if(tags.bytesDisplay_C){
-		float value=size;
-    		value/=tags.blockSize_C;
-    		int out=value;
-    		if(value>out)
-		value++;
-		long int temp_out=value;
-        	printf("%ld\t%s\n", temp_out, workTable);
-	}
-	else{
-        	printf("%ld\t%s\n", size, workTable);
-	}    
+void print(long int size, char *workTable)
+{
+    if (tags.bytesDisplay_C)
+    {
+        float value = size;
+        value /= tags.blockSize_C;
+        int out = value;
+        if (value > out)
+            value++;
+        long int temp_out = value;
+        printf("%ld\t%s\n", temp_out, workTable);
+    }
+    else
+    {
+        printf("%ld\t%s\n", size, workTable);
+    }
 }
 
 /* Reads all files in a given directory and displays identically the way 'du' does */
-long int seekdirec(char* currentdir, int depth){
+long int seekdirec(char *currentdir, int depth)
+{
 
-    if(VERBOSE)
+    if (VERBOSE)
         printf("    [INFO] Received address '%s' ....... OK!\n", currentdir);
 
     char workTable[MAX_SIZE];
     memset(workTable, '\0', MAX_SIZE);
-    strcpy(workTable,currentdir);
+    strcpy(workTable, currentdir);
 
     DIR *d;
     struct dirent *dira;
@@ -165,69 +214,85 @@ long int seekdirec(char* currentdir, int depth){
     long int size = 0;
 
     // First step: Attribute base size of the directory
-    if( !lstat(workTable,&status) ){
+    if (!lstat(workTable, &status))
+    {
         size += sizeAttribution(&status);
-    }  
+    }
 
     d = opendir(workTable);
 
-    if(d == NULL){
+    if (d == NULL)
+    {
         perror("Error: ");
         return -1;
     }
-    else{
-        if(VERBOSE)
+    else
+    {
+        if (VERBOSE)
             printf("    [INFO] Directory '%s' was successfully loaded ....... OK!\n", workTable);
-        
-        while( (dira = readdir(d)) != NULL ){
+
+        while ((dira = readdir(d)) != NULL)
+        {
             strcpy(workTable, currentdir);
             strcat(workTable, "/");
-            if( strcmp(dira->d_name, IGNORE_1) != 0 && strcmp(dira->d_name, IGNORE_2) != 0 ){
-                if( !lstat(strcat(workTable, dira->d_name),&status) ){
+            if (strcmp(dira->d_name, IGNORE_1) != 0 && strcmp(dira->d_name, IGNORE_2) != 0)
+            {
+                if (!lstat(strcat(workTable, dira->d_name), &status))
+                {
                     // [DEBUG] printf("Cycle loaded by process id: %d -- (%s)!\n", getpid(), workTable);
                     // If it is a directory:
-                    if (S_ISDIR(status.st_mode)){
-                        if(VERBOSE)
-                            printf("    [INFO] Directory '%s' is a directory ....... OK! (size: %ld)\n", workTable, status.st_size);                        
+                    if (S_ISDIR(status.st_mode))
+                    {
+                        if (VERBOSE)
+                            printf("    [INFO] Directory '%s' is a directory ....... OK! (size: %ld)\n", workTable, status.st_size);
                         long int returned = 0;
                         returned = createProcess(workTable, depth);
-                        if( !tags.separatedirs_C )
+                        if (!tags.separatedirs_C)
                             size += returned;
                     }
                     // If it is a link:
-                    else if(S_ISLNK(status.st_mode)){
-                        if(VERBOSE)
+                    else if (S_ISLNK(status.st_mode))
+                    {
+                        if (VERBOSE)
                             printf("    [INFO] Directory '%s' is a symbolic link ....... OK!\n", workTable);
-                        if( tags.dereference_C == 0 ){
+                        if (tags.dereference_C == 0)
+                        {
                             // Only considers the file's own size
-                                if( tags.allFiles_C ){                                    
-                                    if( depth > 0 ){
-                                        //printf("[%d]\t", depth);
-                                        print(sizeAttribution(&status), workTable);
-                                    }
-                                }                                
-                                size += sizeAttribution(&status);
+                            if (tags.allFiles_C)
+                            {
+                                if (depth > 0)
+                                {
+                                    //printf("[%d]\t", depth);
+                                    print(sizeAttribution(&status), workTable);
+                                }
+                            }
+                            size += sizeAttribution(&status);
                         }
-                        else{
+                        else
+                        {
                             // Considers the referenced file!
-                                size += dereferenceLink(workTable, depth);
-                        }   
+                            size += dereferenceLink(workTable, depth);
+                        }
                     }
                     // If it is a regular file:
-                    else if (S_ISREG(status.st_mode)){
-                        if(VERBOSE)
-                            printf("    [INFO] Directory '%s' is a regular file ....... OK!\n", workTable);                        
-                        size += sizeAttribution(&status); 
-                        if(tags.allFiles_C){
-                            if( depth > 0 ){
+                    else if (S_ISREG(status.st_mode))
+                    {
+                        if (VERBOSE)
+                            printf("    [INFO] Directory '%s' is a regular file ....... OK!\n", workTable);
+                        size += sizeAttribution(&status);
+                        if (tags.allFiles_C)
+                        {
+                            if (depth > 0)
+                            {
                                 //printf("[%d]\t", depth);
                                 print(sizeAttribution(&status), workTable);
                             }
                         }
                     }
                 }
-                else{
-                    if(VERBOSE)
+                else
+                {
+                    if (VERBOSE)
                         printf("    [FAILURE REPORT] Directory '%s' returned some error ....... ERROR!\n", workTable);
                 }
             }
@@ -235,25 +300,29 @@ long int seekdirec(char* currentdir, int depth){
     }
     closedir(d);
     strcpy(workTable, currentdir);
-    if( depth >= 0 ){
-	if(tags.bytesDisplay_C){
-		float value=size;
-    		value/=tags.blockSize_C;
-    		int out=value;
-    		if(value>out)
-		value++;
-		long int temp_out=value;
-        	printf("%ld\t%s\n", temp_out, workTable);
-	}
-	else{
-        	printf("%ld\t%s\n", size, workTable);
-	}    
+    if (depth >= 0)
+    {
+        if (tags.bytesDisplay_C)
+        {
+            float value = size;
+            value /= tags.blockSize_C;
+            int out = value;
+            if (value > out)
+                value++;
+            long int temp_out = value;
+            printf("%ld\t%s\n", temp_out, workTable);
+        }
+        else
+        {
+            printf("%ld\t%s\n", size, workTable);
+        }
     }
-
+    sleep(2);
     return size;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
     char directoryLine[MAX_SIZE] = DIRECTORY;
     tags.maxDepth_C = CUSTOM_INF;
@@ -261,46 +330,59 @@ int main(int argc, char *argv[]){
     tags.blockSize_C = BLOCK_SIZE_STAT;
 
     // Set up flags
-    for(int i = 1; i < argc; i++){
+    for (int i = 1; i < argc; i++)
+    {
         // allFiles_C (-a or --all)
-        if( strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--all") == 0 ){
+        if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--all") == 0)
+        {
             tags.allFiles_C = 1;
         }
-        else if( strcmp(argv[i], "-b") == 0 || strcmp(argv[i], "--bytes") == 0 ){
+        else if (strcmp(argv[i], "-b") == 0 || strcmp(argv[i], "--bytes") == 0)
+        {
             tags.blockSize_C = 1;
-	    tags.bytesDisplay_C=1;
+            tags.bytesDisplay_C = 1;
         }
-        else if( strcmp(argv[i], "-B") == 0 || strncmp(argv[i], "--block-size=", 13) == 0 || (strstr(argv[i], "-B")) != NULL || (strstr(argv[i], "--block-size=")) != NULL){
-            if( strcmp(argv[i], "-B") == 0 ){
+        else if (strcmp(argv[i], "-B") == 0 || strncmp(argv[i], "--block-size=", 13) == 0 || (strstr(argv[i], "-B")) != NULL || (strstr(argv[i], "--block-size=")) != NULL)
+        {
+            if (strcmp(argv[i], "-B") == 0)
+            {
                 i++;
                 tags.blockSize_C = atoi(argv[i]);
             }
-            if( strncmp(argv[i], "--block-size=", 13) == 0 ){
+            if (strncmp(argv[i], "--block-size=", 13) == 0)
+            {
                 tags.blockSize_C = atoi(argv[i] + 13 * sizeof(char));
             }
-	    char *temp;
-	    if((temp=strstr(argv[i], "-B")) != NULL){
-		temp+=2;
+            char *temp;
+            if ((temp = strstr(argv[i], "-B")) != NULL)
+            {
+                temp += 2;
                 tags.blockSize_C = atoi(temp);
-	    }
-	    if((temp=strstr(argv[i], "--block-size=")) != NULL){
-		temp+=13;
+            }
+            if ((temp = strstr(argv[i], "--block-size=")) != NULL)
+            {
+                temp += 13;
                 tags.blockSize_C = atoi(temp);
-	    }
+            }
         }
-        else if( strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--count-links") == 0 ){
+        else if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--count-links") == 0)
+        {
             // Do thing
         }
-        else if( strcmp(argv[i], "-L") == 0 || strncmp(argv[i], "--dereference", 13) == 0 ){
+        else if (strcmp(argv[i], "-L") == 0 || strncmp(argv[i], "--dereference", 13) == 0)
+        {
             tags.dereference_C = 1;
         }
-        else if( strcmp(argv[i], "-S") == 0 || strncmp(argv[i], "--separate-dirs", 13) == 0 ){
+        else if (strcmp(argv[i], "-S") == 0 || strncmp(argv[i], "--separate-dirs", 13) == 0)
+        {
             tags.separatedirs_C = 1;
         }
-        else if( strncmp(argv[i], "--max-depth=", 12) == 0 ){
+        else if (strncmp(argv[i], "--max-depth=", 12) == 0)
+        {
             tags.maxDepth_C = atoi(argv[i] + 12 * sizeof(char));
         }
-        else{
+        else
+        {
             strcpy(directoryLine, argv[i]);
         }
     }
